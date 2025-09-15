eeb6c98e5ae90e6d57c4bbc4a48f6a3958864bf5
Alex <alexgriesemer@gmail.com>
2025-09-14 20:47:32 -0500

    full chnages

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  server: { port: 5173 },
+  server: { port: 3000 },
   test: {
-    environment: 'jsdom'
+    environment: 'jsdom',
+    setupFiles: ['./src/storage.test.ts'], // optional
   }
 });
